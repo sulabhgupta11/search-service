ed3eb20eb0d309c7564961a6ca1bbf4533271f77
sulabh <sulabhgupta11@gmail.com>
2021-11-10 17:09:56 +0530

    remove cors

--- src/main/java/com/team6/apps/search/config/WebSecurityConfig.java
+++ src/main/java/com/team6/apps/search/config/WebSecurityConfig.java
@@ -58,7 +58,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.csrf().disable().cors().and()
+		httpSecurity.csrf().disable()
 				.authorizeRequests().antMatchers(AUTH_WHITELIST).permitAll().
 				anyRequest().authenticated().
 				and().
